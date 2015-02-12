cb274d3e3cd4b3f6c2ca00af10dfba88b0a1c892
Yojimbo <yojimbo@192-168-1-10.tpgi.com.au>
2015-02-12 23:04:11 +1100

    Test display request

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.get('/gifi', function(request, response) {
+  response.send(request);
+});
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
