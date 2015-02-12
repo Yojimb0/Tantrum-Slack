1306d849e051e51a2fc8625f4067c7b2122b8ff1
Yojimbo <yojimbo@192-168-1-10.tpgi.com.au>
2015-02-12 23:15:22 +1100

    Test Slack call

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+
+// ?token=eZH7rG3VoRbUxOnrkieCAkQL&team_id=T0001&channel_id=C2147483705&channel_name=test&user_id=U2147483697&user_name=Steve&command=/gifi&text=zob
 app.get('/gifi', function(request, response) {
-  response.send(request);
+  response.send('Vous avez demandé un gif en relation avec : '+request.query.text);
+  console.log(request);
+  // JSON.stringify(request)
 });
 
 app.listen(app.get('port'), function() {
