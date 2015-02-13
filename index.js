e16bc48c3888a066481c539427dee686a53931c1
Pierre Pointereau <pierreyves.pointereau@news.com.au>
2015-02-13 14:47:39 +1100

    Setting the request back to Slack

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var request = require('request');
+var config = require('./private-config/config.json');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -9,11 +11,41 @@ app.get('/', function(request, response) {
 });
 
 
+console.log('Slack token: ' + config.token);
+
 // ?token=eZH7rG3VoRbUxOnrkieCAkQL&team_id=T0001&channel_id=C2147483705&channel_name=test&user_id=U2147483697&user_name=Steve&command=/gifi&text=zob
-app.get('/gifi', function(request, response) {
-  response.send('Vous avez demandé un gif en relation avec : '+request.query.text);
-  console.log(request);
-  // JSON.stringify(request)
+app.get('/gifi', function(req, res) {
+	// response.send('Vous avez demandé un gif en relation avec : '+request.query.text);
+	
+	/* REQUEST -----------------------------*/
+	// Set the headers
+	var headers = {
+	    'User-Agent':       'Super Agent/0.0.1',
+	    'Content-Type':     'application/x-www-form-urlencoded'
+	}
+
+	// Configure the request
+	var options = {
+	    url: 'https://slack.com/api/chat.postMessage',
+	    method: 'POST',
+	    headers: headers,
+	    form: {
+	    	'token': config.token,
+	    	'channel': request.query.channel_id,
+	    	// 'channel': '#test',
+	    	'text': 'Vous avez demandé un gif en relation avec : '+request.query.text,
+	    	'username': 'ZeBot'
+	    }
+	}
+
+	// Start the request
+	request(options, function (error, response, body) {
+	    if (!error && response.statusCode == 200) {
+	        // Print out the response body
+	        console.log(body)
+	    }
+	})
+	/* ---------------------------------------*/
 });
 
 app.listen(app.get('port'), function() {
